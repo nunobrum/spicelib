47d2fbbc865cdc15c66ab0c87054f4cf4cf4b4a2
Nuno Brum <nuno.brum@gmail.com>
2023-09-12 10:10:40 +0200

    Small correction identifiied by PyLTSpice issue #120

--- examples/run_montecarlo.py
+++ examples/run_montecarlo.py
@@ -19,7 +19,7 @@ mc.set_parameter_deviation('Vos', 3e-4, 5e-3, 'uniform')  # The keyword 'distrib
 mc.prepare_testbench(1000)  # Prepares the testbench for 1000 simulations
 
 # Finally the netlist is saved to a file
-mc.save_netlist('./testfiles/sallenkey_mc.net')
+mc.save_netlist('./testfiles/sallenkey_mc.asc')
 
 mc.run(100)  # Runs the simulation with splits of 100 runs each
 logs = mc.read_logfiles()   # Reads the log files and stores the results in the results attribute
