a99856190a7e2779bff66dc7961e8c9253464354
lshy <lshy@mail.ustc.edu.cn>
2023-09-28 15:13:03 +0800

    Optimizing error logging: Check executable existence while running simulator.

--- spicelib/simulators/qspice_simulator.py
+++ spicelib/simulators/qspice_simulator.py
@@ -25,7 +25,7 @@ from pathlib import Path
 import logging
 _logger = logging.getLogger("spicelib.QSpiceSimulator")
 
-from ..sim.simulator import Simulator, run_function
+from ..sim.simulator import Simulator, run_function, SpiceSimulatorError
 
 
 class Qspice(Simulator):
@@ -50,11 +50,6 @@ class Qspice(Simulator):
                 break
         else:
             spice_exe = []
-            _logger.error("================== ALERT! ====================")
-            _logger.error("Unable to find the QSPICE executable.")
-            _logger.error("A specific location of the LTSPICE can be set")
-            _logger.error("using the create_from(<location>) class method")
-            _logger.error("==============================================")
 
         process_name = "QSPICE64.exe"
 
@@ -108,6 +103,13 @@ class Qspice(Simulator):
 
     @classmethod
     def run(cls, netlist_file, cmd_line_switches, timeout):
+        if not cls.spice_exe:
+            _logger.error("================== ALERT! ====================")
+            _logger.error("Unable to find the QSPICE executable.")
+            _logger.error("A specific location of the QSPICE can be set")
+            _logger.error("using the create_from(<location>) class method")
+            _logger.error("==============================================")
+            raise SpiceSimulatorError("Simulator executable not found.")
         log_file = Path(netlist_file).with_suffix('.log').as_posix()
         cmd_run = cls.spice_exe + ['-o', log_file] + [netlist_file] + cmd_line_switches
         # start execution
