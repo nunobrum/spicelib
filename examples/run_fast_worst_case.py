ebe38c46195eb4d8deb6a7634018961dc0308779
Nuno Brum <nuno.brum@gmail.com>
2023-11-11 20:24:07 +0100

    Reducing the number of combinations needed, since some of them have no impact on the final result.

--- examples/run_fast_worst_case.py
+++ examples/run_fast_worst_case.py
@@ -8,19 +8,20 @@ from spicelib.simulators.ltspice_simulator import LTspice
 spicelib.set_log_level(logging.INFO)
 
 sallenkey = AscEditor("./testfiles/sallenkey.asc")  # Reads the asc file into memory
-runner = SimRunner(simulator=LTspice, output_folder='./temp_wca', verbose=True)  # Instantiates the runner with a temp folder set
+runner = SimRunner(simulator=LTspice, output_folder='./temp_fwca', verbose=True)  # Instantiates the runner with a temp folder set
 wca = FastWorstCaseAnalysis(sallenkey, runner)  # Instantiates the Worst Case Analysis class
 
 # The following lines set the default tolerances for the components
-# wca.set_tolerance('R', 0.01)  # 1% tolerance
+wca.set_tolerance('R', 0.01)  # 1% tolerance
 wca.set_tolerance('C', 0.1)  # 10% tolerance
 # wca.set_tolerance('V', 0.1)  # 10% tolerance. For Worst Case analysis, the distribution is irrelevant
-# wca.set_tolerance('I', 0.1)  # 10% tolerance. For Worst Case analysis, the distribution is irrelevant
+wca.set_tolerance('I', 0.1)  # 10% tolerance. For Worst Case analysis, the distribution is irrelevant
 # Some components can have a different tolerance
 wca.set_tolerance('R1', 0.05)  # 5% tolerance for R1 only. This only overrides the default tolerance for R1
+wca.set_tolerance('R4', 0.0)  # 5% tolerance for R1 only. This only overrides the default tolerance for R1
 
 # Tolerances can be set for parameters as well.
-wca.set_parameter_deviation('Vos', 3e-4, 5e-3)
+# wca.set_parameter_deviation('Vos', 3e-4, 5e-3)
 
 # Finally the netlist is saved to a file
 wca.save_netlist('./testfiles/sallenkey_fwc.asc')
--- examples/run_worst_case.py
+++ examples/run_worst_case.py
@@ -12,15 +12,16 @@ runner = SimRunner(simulator=LTspice, output_folder='./temp_wca', verbose=True) 
 wca = WorstCaseAnalysis(sallenkey, runner)  # Instantiates the Worst Case Analysis class
 
 # The following lines set the default tolerances for the components
-# wca.set_tolerance('R', 0.01)  # 1% tolerance
+wca.set_tolerance('R', 0.01)  # 1% tolerance
 wca.set_tolerance('C', 0.1)  # 10% tolerance
 # wca.set_tolerance('V', 0.1)  # 10% tolerance. For Worst Case analysis, the distribution is irrelevant
-# wca.set_tolerance('I', 0.1)  # 10% tolerance. For Worst Case analysis, the distribution is irrelevant
+wca.set_tolerance('I', 0.1)  # 10% tolerance. For Worst Case analysis, the distribution is irrelevant
 # Some components can have a different tolerance
 wca.set_tolerance('R1', 0.05)  # 5% tolerance for R1 only. This only overrides the default tolerance for R1
+wca.set_tolerance('R4', 0.0)  # 5% tolerance for R1 only. This only overrides the default tolerance for R1
 
 # Tolerances can be set for parameters as well.
-wca.set_parameter_deviation('Vos', 3e-4, 5e-3)
+# wca.set_parameter_deviation('Vos', 3e-4, 5e-3)
 
 # Finally the netlist is saved to a file
 wca.save_netlist('./testfiles/sallenkey_wc.asc')
@@ -32,6 +33,10 @@ logs.export_data('./temp_wca/data.csv')  # Exports the data to a csv file
 print("Worst case results:")
 for param in ('fcut', 'fcut_FROM'):
     print(f"{param}: min:{logs.min_measure_value(param)} max:{logs.max_measure_value(param)}")
+
+## All components sensitivity
+sens = wca.make_sensitivity_analysis('fcut', '*')  # Makes the sensitivity analysis for all components
+print(sens)
 
 wca.cleanup_files()  # Deletes the temporary files
 
