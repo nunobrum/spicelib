f6b0e8e8db289dff946fadcbb6dcb481b6c13e3f
hans boot <hansboot020@yahoo.com>
2024-09-28 13:20:15 +0200

    mu was not translated

--- unittests/test_asc_editor.py
+++ unittests/test_asc_editor.py
@@ -76,7 +76,6 @@ class ASC_Editor_Test(unittest.TestCase):
         self.edt.save_netlist(temp_dir + 'test_components_output_1.asc')
         self.equalFiles(temp_dir + 'test_components_output_1.asc', golden_dir + 'test_components_output_1.asc')
 
-
     def test_parameter_edit(self):
         self.assertEqual(self.edt.get_parameter('TEMP'), '0', "Tested TEMP Parameter")  # add assertion here
         self.edt.set_parameter('TEMP', 25)
@@ -100,6 +99,67 @@ class ASC_Editor_Test(unittest.TestCase):
         self.edt.remove_Xinstruction(r"\.save\sI\(.*\)")  # removes all .save instructions for currents
         self.edt.save_netlist(temp_dir + 'test_instructions_output_2.asc')
         self.equalFiles(temp_dir + 'test_instructions_output_2.asc', golden_dir + 'test_instructions_output_2.asc')
+
+    def test_subcircuit1_edit(self):
+        edt2 = spicelib.editor.asc_editor.AscEditor(test_dir + "top_circuit.asc")
+        self.assertEqual(edt2.get_subcircuit('X1').get_components(), ['C1', 'C2', 'L1'], "Subcircuit component list")
+        self.assertEqual(edt2.get_component_value("X1:L1"), "1µ", "Subcircuit Value for X1:L1, direct")
+        self.assertEqual(edt2.get_subcircuit('X1').get_component_value('L1'), "1µ", "Subcircuit Value for X1:L1, indirect")
+        edt2.set_component_value("X1:L1", 2e-6)
+        self.assertEqual(edt2['X1:L1'].value_str, "2u", "Subcircuit Value for X1:L1, after change")
+        edt2['R1'].value = 11
+        edt2.set_parameter("V1", "PULSE(0 1 1n 1n 1n {0.5/freq} {1/freq} 10)")
+        edt2.set_parameters(freq=1E6)
+        edt2["X1:L1"].value = '1µH'
+        print(f"X1:L1: {edt2['X1:L1'].value_str}")
+        print(f"X1:L1: {edt2['X1:L1'].value}")
+        edt2["X1:C1"].value = 22e-9
+        print(f"X1:C1: {edt2['X1:C1'].value_str}")
+        print(f"X1:C2: {edt2.get_component_floatvalue('X1:C2')}")
+        print(f"R2: {edt2['R2'].value_str}")
+        edt2.set_parameters(
+            test_exiting_param_set1=24,
+            test_exiting_param_set2=25,
+            test_exiting_param_set3=26,
+            test_exiting_param_set4=27,
+            test_add_parameter=34.45, )
+        S = edt2.get_subcircuit('X1')
+        S.asc_file_path = temp_dir + "subcircuit_edit.asc"  # Only for test purposes
+        edt2.save_netlist(temp_dir + "top_circuit_edit.asc")
+        self.equalFiles(temp_dir + "top_circuit_edit.asc", golden_dir + "top_circuit_edit.asc")
+        
+# E = AscEditor('testfiles\\top_circuit.asc')
+# print(E.get_components())
+# print(E.get_components('R'))
+# print(E.get_subcircuit('X1').get_components())
+# E.set_component_value("X1:L1", 2e-6)
+# print(E['R1'].value)
+# print("Setting R1 to 10k")
+# E['R1'].value = 11
+# print("Setting parameter I1 1.23k")
+# E.set_parameter("V1", "PULSE(0 1 1n 1n 1n {0.5/freq} {1/freq} 10)")
+# print(E.get_parameter('V1'))
+# print("Setting frequency to 1MHz")
+# E.set_parameters(freq=1E6)
+# print("Setting XX1:L1 to 1µH")
+# E["X1:L1"].value = '1µH'
+# print("Setting XX1:C1 to 22nF")
+# E["X1:C1"].value = 22e-9
+# print("Setting XX1:C2 to 120nF")
+# E["X1:C2"].value = '120n'
+# print(E["X1:C1"].value)
+# print(E.get_component_floatvalue("X1:C2"))
+# print(E["X1:L1"].value)
+# print(E["R2"].value_str)
+# E.set_parameters(
+#     test_exiting_param_set1=24,
+#     test_exiting_param_set2=25,
+#     test_exiting_param_set3=26,
+#     test_exiting_param_set4=27,
+#     test_add_parameter=34.45, )
+# S = E.get_subcircuit('X1')
+# S.asc_file_path = "testfiles\\subcircuit_edit.asc" # Only for test purposes
+# E.save_netlist("testfiles\\top_circuit_edit.asc")
 
     def equalFiles(self, file1, file2):
         with open(file1, 'r') as f1:
