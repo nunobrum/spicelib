59c2ad4a5d36a789eb7abc266f6d1b045e487fd6
hans boot <hansboot020@yahoo.com>
2025-02-08 10:24:32 +0100

    simplify handling of exe_log

--- spicelib/sim/run_task.py
+++ spicelib/sim/run_task.py
@@ -100,16 +100,7 @@ class RunTask(threading.Thread):
 
         self.start_time = clock_function()
         self.print_info(_logger.info, ": Starting simulation %d: %s" % (self.runno, self.netlist_file))
-        if self.exe_log:
-            self.exe_log_file = self.netlist_file.with_suffix('.exe.log')
-
-        # start execution
-        if self.exe_log:
-            with open(self.exe_log_file, "w") as outfile:
-                self.retcode = self.simulator.run(self.netlist_file.absolute().as_posix(), self.switches, self.timeout,
-                                                  stdout=outfile, stderr=subprocess.STDOUT)
-        else:
-            self.retcode = self.simulator.run(self.netlist_file.absolute().as_posix(), self.switches, self.timeout)
+        self.retcode = self.simulator.run(self.netlist_file.absolute().as_posix(), self.switches, self.timeout, exe_log=self.exe_log)
         self.stop_time = clock_function()
         # print simulation time with format HH:MM:SS.mmmmmm
 
