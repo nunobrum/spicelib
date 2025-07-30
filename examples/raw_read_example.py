8108eba2c4e93a1b4b71b0e152d58806b029d9b5
hans boot <hansboot020@yahoo.com>
2025-07-30 20:06:39 +0200

    improve doc

--- examples/raw_read_example.py
+++ examples/raw_read_example.py
@@ -3,6 +3,7 @@ from spicelib import RawRead
 from matplotlib import pyplot as plt
 
 # read a raw file that has only 1 data set/plot in it, but has multiple steps
+
 rawfile = RawRead("./testfiles/TRAN - STEP.raw")
 
 print(rawfile.get_trace_names())
@@ -19,22 +20,23 @@ plt.legend()  # order a legend
 plt.show()
 
 # read a raw file that has multiple data sets/plots in it
+
 raw = RawRead("./testfiles/noise_multi.bin.raw")
 print(raw.get_plot_names())            # names of all the plots in the file
 print(raw.get_trace_names())           # names of all the traces of the first plot in the file
 print(raw.plots[0].get_trace_names())  # same as above
 print(raw.plots[1].get_trace_names())  # names of all the traces of the second plot in the file
 
-# Gets the frequency axis of the first plot (same as raw.plots[0].get_trace('frequency'))
 x = raw.get_trace('frequency')  # could have used raw.get_axis() as well here
 y = raw.get_trace('onoise_spectrum')
 plt.plot(x.get_wave(), y.get_wave(), label='noise spectrum')
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Noise Spectrum (V/√Hz)')
+plt.ylabel('Noise (V/√Hz)')
 plt.yscale('log')
 plt.xscale('log')
-plt.legend()  # order a legend
+plt.legend()
 plt.show()
 
+# and get the integrated noise from the second part in the file
 total = raw.plots[1].get_trace('v(onoise_total)')
 print(f"Total Integral noise: {total.get_wave()[0]} V") 
